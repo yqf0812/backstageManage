ecf1c5c7660ed8915c2e3dcf346cc106cdc7c7fd
yangqingfeng <yangqingfeng@ztessc.com.cn>
2020-10-23 16:08:03 +0800

    配置rem

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-10-23 15:20:58
+ * @LastEditTime: 2020-10-23 15:45:59
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \backstageManage\src\App.vue
+-->
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>123</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
+html {
+  overflow: hidden;
+}
+html,
+body {
+  height: 100%;
+  background-color: #f9fbff;
+  padding: 0 !important;
+  font-size: 14px;
+}
+textarea {
+  font-family: inherit;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
 </style>
