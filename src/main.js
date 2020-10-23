bb491504f5780e77ed1a57faa5c81da99f56ff37
yangqingfeng <yangqingfeng@ztessc.com.cn>
2020-10-23 16:31:03 +0800

    配置基础工具

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
+/*
+ * @Author: your name
+ * @Date: 2020-10-23 15:20:58
+ * @LastEditTime: 2020-10-23 16:30:34
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \backstageManage\src\main.js
+ */
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: your name
+ * @Date: 2020-10-23 16:21:33
+ * @LastEditTime: 2020-10-23 16:29:02
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \backstageManage\src\router\index.js
+ */
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+    routes:[
+      
+    ]
+  })
