c607fad2efbe5b7f0eef2a853e8c827424a9f58e
yangqingfeng <yangqingfeng@ztessc.com.cn>
2020-10-23 17:33:00 +0800

    登录页背景

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,14 +1,14 @@
 <!--
  * @Author: your name
  * @Date: 2020-10-23 16:58:34
- * @LastEditTime: 2020-10-23 17:15:20
+ * @LastEditTime: 2020-10-23 17:29:29
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \backstageManage\src\views\login\login.vue
 -->
 <template>
   <div class="login">
-      
+      <div class="mask"></div>
   </div>
 </template>
 
@@ -23,5 +23,15 @@ export default {
     height: 100%;
     background-image: url('./../../assets/images/background.png');
     background-size: 100% 100%;
+    background-repeat: no-repeat;   
+    background-attachment: fixed;   
+    overflow: hidden; 
+}
+.mask {
+    width: 100%;
+    height: 100%;
+    background: inherit;   
+    filter: blur(50px);   
+    filter: progid:DXImageTransform.Microsoft.Blur(PixelRadius=4, MakeShadow=false);
 }
 </style>
