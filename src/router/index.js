cdf6493b47cfd1b530ee074b43b634df03c63802
yangqingfeng <yangqingfeng@ztessc.com.cn>
2020-10-23 17:17:54 +0800

    添加登录页

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-10-23 16:21:33
- * @LastEditTime: 2020-10-23 16:29:02
+ * @LastEditTime: 2020-10-23 17:06:19
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \backstageManage\src\router\index.js
@@ -9,10 +9,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Login from './../views/login/login'
+
 Vue.use(VueRouter)
 
 export default new VueRouter({
     routes:[
-      
+        { path: '/login', component: Login },
+        { path: '', redirect: 'Login' }
     ]
   })
---
+++ src/views/login/login.vue
@@ -0,0 +1,27 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-10-23 16:58:34
+ * @LastEditTime: 2020-10-23 17:15:20
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \backstageManage\src\views\login\login.vue
+-->
+<template>
+  <div class="login">
+      
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Login'
+}
+</script>
+
+<style>
+.login {
+    height: 100%;
+    background-image: url('./../../assets/images/background.png');
+    background-size: 100% 100%;
+}
+</style>
