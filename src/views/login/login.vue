1ff3e3389fdad87ddadaa396843db28d6de76fdf
yangqingfeng <yangqingfeng@ztessc.com.cn>
2020-11-05 21:10:59 +0800

    添加登录页输入框

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2020-10-23 16:58:34
- * @LastEditTime: 2020-10-23 17:29:29
+ * @LastEditTime: 2020-11-05 21:09:14
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \backstageManage\src\views\login\login.vue
@@ -9,12 +9,25 @@
 <template>
   <div class="login">
       <div class="mask"></div>
+      <div class="login-box">
+          <div class="input-box">
+            <el-input class="el-input__style" v-model="user" name="user" placeholder="请输入内容"></el-input>
+            <el-input class="el-input__style" v-model="passowrd" name="password" placeholder="请输入内容"></el-input>
+          </div>
+      </div>
+      <!-- <el-input v-model="input" placeholder="请输入内容"></el-input> -->
   </div>
 </template>
 
 <script>
 export default {
-    name: 'Login'
+    name: 'Login',
+    data() {
+        return {
+            user: '',
+            password: ''
+        }
+    }
 }
 </script>
 
@@ -26,12 +39,38 @@ export default {
     background-repeat: no-repeat;   
     background-attachment: fixed;   
     overflow: hidden; 
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 .mask {
-    width: 100%;
-    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 19.2rem;
+    height: 10.8rem;
     background: inherit;   
     filter: blur(50px);   
     filter: progid:DXImageTransform.Microsoft.Blur(PixelRadius=4, MakeShadow=false);
+}
+.login-box {
+    height: 50%;
+    width: 50%;
+    border-radius: 0.1rem;
+    z-index: 999;
+    margin-top: -1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.input-box {
+    margin-top: -0.2rem;
+    width: 30%;
+}
+.input-box .el-input__inner {
+    margin-top: 0.2rem;
+    background-color: transparent;
+    color: #fff;
 }
 </style>
